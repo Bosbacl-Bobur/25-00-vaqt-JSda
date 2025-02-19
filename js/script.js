c6be11dbf9dcb5ad3fb0b653bdbda6f66a227dbb
Sening isming <Emailing>
2025-02-19 15:04:11 +0500

    ozginasini tugatdim

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const starEl = document.getElementById("start") 
+const stopEl = document.getElementById("stop") 
+const timerEl = document.getElementById("timer") 
+
+starEl.addEventListener("click", startTimer)
+stopEl.addEventListener("click", stopTimer)
+
+
+let interval;
+let timeLeft = 1500;
+
+function updateTimer(){
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let formattedTime = ` ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+    timerEl.innerHTML = formattedTime;
+}
+function startTimer(){
+    interval = setInterval(()=>{
+        timeLeft--;
+        updateTimer();
+        if (timeLeft === 0) {
+            clearInterval(interval);
+            alert("Time's up!");
+            timeLeft = 1500;
+        }
+    }, 1000)
+}
+function stopTimer(){
+    clearInterval(interval); 
+}
